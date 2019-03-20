8948a63d80d177ecb9d2ec89346a781230cfd2a9
DerJP <0x13156e9@gmail.com>
2019-03-20 21:08:38 +0100

    Fixed regular expressions

--- dist/config.js
+++ dist/config.js
@@ -1,15 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const RANKS = 'Owner|Admin|TS-Admin|Developer|Moderator|Supporter|Designer|Content|Freund+|Freund|YouTuber+|Youtuber+|Supreme|Champ|Champion|Griefer|Titan|Legende|YouTuber|Youtuber|Ultra|Premium|Spieler';
 exports.config = {
     NORMAL_COOLDOWN: 3050,
     SLOW_COOLDOWN: 4050,
     PORTAL_COOLDOWN: 12000,
     PORTAL_TIMEOUT: 30000,
-    MSG_REGEXP: new RegExp(`^\\[(${RANKS}) \\| ([A-Za-z0-9_]{1,16}) -> mir\\] (.+)$`),
-    CHATMODE_ALERT_REGEXP: new RegExp(`^Der Chat wurde von (${RANKS}) \\| ([A-Za-z0-9_]{1,16}) (.+)\\.$`),
+    MSG_REGEXP: /^\[(\w+) \| (\w{1,16}) -> mir\] (.+)$/,
+    CHATMODE_ALERT_REGEXP: /^Der Chat wurde von (\w+) \| (\w{1,16}) (.+)\.$/,
     SLOWCHAT_ALERT_REGEXP: /^Du kannst nur jede 3 Sekunden schreiben.$/,
     COMMANDSPAM_ALERT_REGEXP: /^Bitte unterlasse das Spammen von Commands!/,
-    PAY_REGEXP: new RegExp(`^[§0-9a-fA-Fk-oK-OrR]{2,}(${RANKS}) \\| ([A-Za-z0-9_]{1,16}) §ahat dir \\$(\\S+) gegeben\\.$`),
+    PAY_REGEXP: /^[§0-9a-fA-Fk-oK-OrR]{2,}(\w+) \| (\w{1,16}) §ahat dir \$(\S+) gegeben\.$/,
 };
 //# sourceMappingURL=config.js.map
