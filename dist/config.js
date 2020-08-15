e84125a990bb0a446b4f5e5f0cbff720ecb51e65
Neocraftr <51837582+Neocraftr@users.noreply.github.com>
2020-08-16 00:40:50 +0200

    Updated regex to new chat style.

--- dist/config.js
+++ dist/config.js
@@ -8,13 +8,13 @@ exports.config = {
     SLOW_COOLDOWN: 4050,
     PORTAL_COOLDOWN: 12000,
     PORTAL_TIMEOUT: 60000,
-    MSG_REGEXP: /^\[(\w+) \u2503 (\u007E?\w{1,16}) -> mir\] (.+)$/,
-    CHATMODE_ALERT_REGEXP: /^Der Chat wurde von (\w+) \| (\w{1,16}) (.+)\.$/,
+    MSG_REGEXP: /^\[(\w+) \u2503 (\u007E?\u0021?\w{1,16}) -> mir\] (.+)$/,
+    CHATMODE_ALERT_REGEXP: /^Der Chat wurde von (\w+) \u2503 (\u007E?\u0021?\w{1,16}) (.+)\.$/,
     SLOWCHAT_ALERT_REGEXP: /^Du kannst nur jede 3 Sekunden schreiben.$/,
     COMMANDSPAM_ALERT_REGEXP: /^Bitte unterlasse das Spammen von Commands!$/,
-    PAY_REGXP: /^(.+) \u2503 (\u007E?\w{1,16}) hat dir \$(\S+) gegeben\.$/,
+    PAY_REGXP: /^(.+) \u2503 (\u007E?\u0021?\w{1,16}) hat dir \$(\S+) gegeben\.$/,
     REDSTONE_REGEXP: /^- Redstone (?:ist wieder )?(\w+)\!?$/,
     ITEMCLEAR_REGEXP: /^\[GrieferGames\] Warnung: Items auf dem Boden werden in (\w{2}) Sekunden entfernt\!$/,
-    CODED_PAY_REGEXP: /^(.+) \| (.+) §ahat dir \$(\S+) gegeben\.$/,
+    CODED_PAY_REGEXP: /^(.+) \u2503 (.+) §ahat dir \$(\S+) gegeben\.$/,
 };
 //# sourceMappingURL=config.js.map
